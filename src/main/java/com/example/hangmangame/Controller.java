c69a0743d0896ea2221666fc8bd45e33e748772b
jtdev632 <jaccek.bid@gmail.com>
2024-10-29 23:20:11 +0100

    first commit

---
+++ src/main/java/com/example/hangmangame/Controller.java
@@ -0,0 +1,153 @@
+package com.example.hangmangame;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.Alert;
+import javafx.scene.control.Button;
+import javafx.scene.control.ButtonType;
+import javafx.scene.control.TextInputDialog;
+import javafx.scene.media.AudioClip;
+import javafx.scene.layout.FlowPane;
+import javafx.scene.layout.HBox;
+import javafx.scene.control.Label;
+import javafx.application.Platform;
+
+
+import java.util.Arrays;
+import java.util.Optional;
+
+public class Controller {
+
+    @FXML
+    private HBox wordDisplay;
+
+    @FXML
+    private FlowPane letterGrid;
+    private String wordToGuess;
+    private char[] guessedWord;
+    private final int maxMistakes = 10;
+    private int mistakes;
+    private final AudioClip clickSound = new AudioClip(getClass().getResource("/com/example/hangmangame/sounds/sound.wav").toString());
+    public void initialize() {
+        promptForWord();
+        displayWordPlaceholders();
+        setupLetterButtons();
+    }
+
+    private void resetGame(String message) {
+        Alert alert = new Alert(Alert.AlertType.CONFIRMATION, message +"Would you like to restart the entire game?", ButtonType.YES, ButtonType.NO);
+        Optional<ButtonType> result = alert.showAndWait();
+
+        if (result.isPresent() && result.get() == ButtonType.YES) {
+            Main.restart();
+        } else {
+            Platform.exit();
+        }
+    }
+
+
+    public boolean containsDigits(String input) {
+        for(int i = 0; i < input.length(); i++){
+            char a = input.charAt(i);
+            if(!Character.isLetter(input.charAt(i))){
+                return false;
+            }
+        }
+        return true;
+    }
+    private void promptForWord() {
+        TextInputDialog dialog = new TextInputDialog();
+        dialog.setTitle("Enter Word");
+        dialog.setHeaderText("Set the word to guess");
+        dialog.setContentText("Please enter a word:");
+
+        Optional<String> result = dialog.showAndWait();
+        if ((result.isPresent() && !result.get().trim().isEmpty()) && containsDigits(result.get())) {
+            wordToGuess = result.get().trim().toUpperCase();
+            guessedWord = new char[wordToGuess.length()];
+            Arrays.fill(guessedWord,'_');
+        } else {
+            showAlert("Invalid input. Please restart the game and enter a valid word.");
+        }
+    }
+
+
+    private void displayWordPlaceholders() {
+        wordDisplay.getChildren().clear();
+
+        for (char letter : guessedWord) {
+            Label label = new Label(String.valueOf(letter));
+            label.setStyle("-fx-font-size: 24px; -fx-padding: 5;");
+            wordDisplay.getChildren().add(label);
+        }
+    }
+
+
+    private void showAlert(String message) {
+        Alert alert = new Alert(Alert.AlertType.ERROR, message, ButtonType.OK);
+        alert.showAndWait();
+    }
+
+
+    private void setupLetterButtons() {
+
+        for (javafx.scene.Node node : letterGrid.getChildren()) {
+            if (node instanceof Button) {
+                Button letterButton = (Button) node;
+                letterButton.setOnAction(event -> {
+                    clickSound.play();
+                    String letter = letterButton.getText();
+                    processGuess(letter.charAt(0));
+                    letterButton.setDisable(true);
+
+                });
+            }
+        }
+    }
+    public void enableAllLetterButtons() {
+        for(javafx.scene.Node node : letterGrid.getChildren()){
+            if(node instanceof Button){
+                Button button = (Button) node;
+                button.setDisable(false);
+                button.setStyle("");
+                button.setOnAction(null);
+
+            }
+        }
+
+    }
+
+
+    private void processGuess(char guessedLetter) {
+        boolean correctGuess = false;
+
+
+
+        for (int i = 0; i < wordToGuess.length(); i++) {
+            if (wordToGuess.charAt(i) == guessedLetter) {
+                guessedWord[i] = guessedLetter;
+                displayWordPlaceholders();
+                correctGuess = true;
+            }
+        }
+
+        if (correctGuess) {
+
+            if (isWordFullyGuessed()) {
+                resetGame("Congratulations, you won the game.");
+            }
+        } else {
+
+            showAlert("Wrong character, Your mistakes: " + (++this.mistakes));
+            if(mistakes >= maxMistakes){
+                resetGame("Game Over! You've made too many mistakes.");
+            }
+        }
+
+    }
+    private boolean isWordFullyGuessed(){
+        for(char letter: guessedWord){
+            if(letter == '_') return false;
+        }
+        return true;
+    }
+}
---
+++ src/main/java/com/example/hangmangame/Main.java
@@ -0,0 +1,49 @@
+package com.example.hangmangame;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+import javafx.application.Platform;
+
+public class Main extends Application {
+
+    private static Stage primaryStage;
+
+    @Override
+    public void start(Stage stage) throws Exception {
+        primaryStage = stage;
+        loadMainScene();
+    }
+
+    private void loadMainScene() throws Exception {
+        FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource("view.fxml"));
+        Scene scene = new Scene(fxmlLoader.load(), 1280, 720);
+
+
+        scene.getStylesheets().add(getClass().getResource("application.css").toExternalForm());
+
+        primaryStage.setTitle("Hangman Game");
+        primaryStage.setScene(scene);
+        primaryStage.show();
+    }
+
+    public static void restart() {
+        Platform.runLater(() -> {
+            try {
+
+                primaryStage.close();
+
+                Main newApp = new Main();
+                Stage newStage = new Stage();
+                newApp.start(newStage);
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+        });
+    }
+
+    public static void main(String[] args) {
+        launch();
+    }
+}
